84b70e481866c8b2db63fb1a84c4cc49f983d48c
Sagit Gutierrez <jozaguts@gmail.com>
2022-04-26 16:30:18 -0500

    rename table seeder

--- database/seeders/RolesPermissionsTableSeeder.php
+++ database/seeders/RolesPermissionsTableSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
-class RolesPermissionsTableseeder extends Seeder
+class RolesPermissionsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
