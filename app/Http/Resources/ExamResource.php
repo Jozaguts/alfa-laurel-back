62f938bc4651bb9dc5700b9dab0c5d22a3da4c9e
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-02 19:30:49 -0500

    fix: real answer returned

--- app/Http/Resources/ExamResource.php
+++ app/Http/Resources/ExamResource.php
@@ -20,13 +20,13 @@ class ExamResource extends JsonResource
          'user_id' => $this->user_id,
          'subject_id' => $this->subject_id,
          'questions' => array_map(function($question) {
+
              return [
                  'id' => $question['id'],
                  'number' => $question['number'],
                  'level' => $question['level'],
                  'exam_id' => $question['exam_id'],
-                 'answer' => 1, //todo se neceta enviar el valor 1 , 2 0 3 para determnar la respuesta en el formulario
-                 //lo puedes tomar desde el request llega en int
+                 'answer' => $question['answer'],
                  'question' => $question['question'],
                  'options' => array_filter($this->options->toArray(), function($option) use ($question) {
                      return $question['id'] === $option['question_id'];
