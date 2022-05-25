ad907b89ca25a630c151d0ba9eb2283ec3832737
Sagit Gutierrez <jozaguts@gmail.com>
2022-05-25 00:19:34 -0500

    rutas protected

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,13 @@ Route::middleware('auth:sanctum')->group( function () {
         $user = User::with(['roles','permissions'])->where('id', $request->user()->id)->first();
         return response()->json($user);
     });
-
     Route::apiResource('roles', App\Http\Controllers\Api\V1\RoleController::class);
     Route::apiResource('permissions', App\Http\Controllers\Api\V1\PermissionController::class);
     Route::apiResource('users', App\Http\Controllers\Api\V1\UserController::class);
     Route::apiResource('subjects', App\Http\Controllers\Api\V1\SubjectController::class);
     Route::post('examenes/question',[App\Http\Controllers\Api\V1\ExamenController::class,'deleteQuestion']);
-
+    Route::post('examenes-aplicados',[App\Http\Controllers\Api\V1\ExamenController::class,'appliedExams']);
+    Route::apiResource('respuestas', App\Http\Controllers\Api\V1\AnswerController::class);
+    Route::apiResource('examenes', App\Http\Controllers\Api\V1\ExamenController::class);
 });
-Route::apiResource('examenes', App\Http\Controllers\Api\V1\ExamenController::class);
-Route::apiResource('respuestas', App\Http\Controllers\Api\V1\AnswerController::class);
 
