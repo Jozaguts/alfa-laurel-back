f49f28eb2646c536635df739412a5e91a7147662
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-28 20:59:17 -0500

    role permissin removed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(RolesPermissionsTableSeeder::class);
+//        $this->call(RolesPermissionsTableSeeder::class);
     }
 }
