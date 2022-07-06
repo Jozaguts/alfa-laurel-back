bb1733456711f35858b91b016093b3519d0346bc
Sagit Gutierrez <jozaguts@gmail.com>
2022-07-05 22:49:59 -0500

    Rule unique and whereNull

--- app/Http/Requests/SubjectStoreRequest.php
+++ app/Http/Requests/SubjectStoreRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class SubjectStoreRequest extends FormRequest
 {
@@ -23,8 +24,13 @@ class SubjectStoreRequest extends FormRequest
      */
     public function rules()
     {
+        $data = $this->request->all();
         return [
-            'name' =>'required|string',
+            'name' =>[
+                'required',
+                'string',
+                Rule::unique('subjects','name')->whereNull('deleted_at')
+            ]
         ];
     }
 
